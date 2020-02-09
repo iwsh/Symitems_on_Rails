cb7067abb2f26ec5a846ea795356a504881c090d
yuki sato <yuki3100705@gmail.com>
2020-02-09 16:39:43 +0900

    getSchedule_complete

--- app/controllers/acsessdbs_controller.rb
+++ app/controllers/acsessdbs_controller.rb
@@ -1,18 +1,15 @@
 class AcsessdbsController < ApplicationController
   def getSchedule
-    @displayYear = '2020' #TODO:しげから受け取る表示年
-    @displayMonth = '02' #TODO:しげから受け取る表示月
+    @displayYear = 2020 #TODO:しげから受け取る表示年、数値型で渡される想定
+    @displayMonth = 2 #TODO:しげから受け取る表示月、数値型で渡される想定
 
-    # @lastDay = Date.new(@displayYear, @displayMonth, 0).day;
-    @lastDay = '29' #TODO:暫定対応、月の日数の取得方法検討中
+    @lastDay = Date.new(@displayYear, @displayMonth, -1).strftime
+    @dateFrom = format("#{@displayYear}-%02d-01", @displayMonth)
+    @dateTo = format("#{@displayYear}-%02d-#{@lastDay}", @displayMonth)
 
-    @getDate = Hash.new
-    @getDate[:from] = @displayYear + '-' + @displayMonth + '-01' #TODO:強引すぎるからきれいな書き方を検討したい
-    @getDate[:to] = @displayYear + '-' + @displayMonth + '-' + @lastDay #TODO:強引すぎるからきれいな書き方を検討したい
-
-    @schedule = Hash.new
-    @schedule = Schedule.joins(:scheduleContents).select("schedules.*, scheduleContents.*").where("date BETWEEN ? AND ?", @getDate[:month], @getDate[:to])
-    @schedules = Schedule.all #TODO:schedule_contentの情報が取れない、検討中
+    Schedule.joins(:schedule_content)
+    @schedules = Hash.new
+    @schedules = Schedule.where("date BETWEEN ? AND ?", @dateFrom, @dateTo)
   end
 
   def updateSchedule
@@ -23,6 +20,5 @@ class AcsessdbsController < ApplicationController
 
   def deleteSchedule
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'acsessdbs/getSchedule'
+  get 'acsessdbs/updateSchedule'
+  get 'acsessdbs/insertSchedule'
+  get 'acsessdbs/deleteSchedule'
+  # get "acsessdbs/getSchedule/:date" => "calenders#calender"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
