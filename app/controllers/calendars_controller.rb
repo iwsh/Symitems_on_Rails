54c1c056d2a216cc5aa19e510080acaf4d90964f
tomo0119shige <tomo0119shige@gmail.com>
2020-03-04 21:51:23 +0900

    success!

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -21,8 +21,8 @@ class CalendarsController < StabAccessSchedulesController
     #user_idとyear/month('%Y/%m')を使ってgetSchedule?
     #下のSQLの結果がもらえるイメージ（表示だけならこれで足りるが、更新など考えるとid系も必要か？）
     #select strftime('%d', date("schedule"."date")) as date, "schedule_content"."title", "schedule_content"."started_at", "schedule_content"."ended_at", "schedule_content"."detail" from "schedule_content" inner join "schedule" on "schedule_content"."id" = "schedule"."content_id" where "schedule"."user_id" = ? and strftime('%Y/%m', date("schedule"."date")) = ? order by "schedule_content"."started_at", "schedule_content"."ended_at";
-    #@userId = 10
-    #@schedules = AccessSchedulesController.new.getSchedule(@userId,@year,@month)
+    @userId = 2
+    @schedules = StabAccessSchedulesController.new.getSchedule(@userId,@year,@month)
   end
 
   def inputSchedule #GOTO
--- app/controllers/stab_access_schedules_controller.rb
+++ app/controllers/stab_access_schedules_controller.rb
@@ -1,5 +1,20 @@
 class StabAccessSchedulesController < ApplicationController
   def getSchedule(userId, year, month)
-    
+    testSchedule1 = Hash.new
+    testSchedule1[:id] = 1
+    testSchedule1[:date] = '2020-02-05'
+    testSchedule1[:schedule_content_id] = 1
+    testSchedule1[:schedule_content_title] = '第12回シミテムズ'
+    testSchedule2 = Hash.new
+    testSchedule2[:id] = 2
+    testSchedule2[:date] = '2020-02-11'
+    testSchedule2[:schedule_content_id] = 3
+    testSchedule2[:schedule_content_title] = 'スノーボード'
+    @getSchedules = Hash.new
+    if userId == 2 && year == 2020 && month == 2
+      @getSchedules[5] = testSchedule1
+      @getSchedules[11] = testSchedule2
+    end
+    return @getSchedules
   end
 end
