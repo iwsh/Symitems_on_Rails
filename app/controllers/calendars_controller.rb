21f5adc3aea54a402e8d50a5d39fbbd6b976f093
kegot <510kei.goto@gmail.com>
2020-03-07 18:11:14 +0900

    add schedule edit form

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,20 +19,47 @@ class CalendarsController < ApplicationController
   end
 
   def inputSchedule
+    @schedule = Schedule.new 
   end
 
   def registerSchedule
-    if params[:schedule_id].present?
-      @schedule = Schedule.find_by(:schedule_id => params[:schedule_id])
+    if params[:id].present?
+      @schedule = Schedule.find_by(:id => params[:id])
     else
       @schedule = Schedule.new
       @schedule.title = params[:schedule][:title]
       @schedule.stardted_at = params[:schedule][:stardted_at]
       @schedule.ended_at = params[:schedule][:ended_at]
       @schedule.detail = params[:schedule][:detail]
+      @schedule.save
       redirect_to '/calendar' 
     end
   end
+
+  def edit
+# test
+    if params[:schedule_id].to_i == 1
+      @schedule = Schedule.new
+      @schedule.title = 'タイトル!'
+      @schedule.stardted_at = '2020/02/09 20:00:00'
+      @schedule.ended_at = '2020/02/09 21:00:00'
+      @schedule.detail = 'ガストでお勉強する'
+    elsif params[:schedule_id].to_i == 2
+      @schedule = Schedule.new
+      @schedule.title = 'タイトル2'
+      @schedule.stardted_at = '2020/02/09 20:00:00'
+      @schedule.ended_at = '2020/02/09 21:00:00'
+      @schedule.detail = '家でお勉強する'
+    end
+#    @schedule = Schedule.find(params[:id])
+  end
+
+  def update
+    @schedule = Schedule.find(params[:id])
+    @schedule.update(params.require(:schedule).permit(:title, :stardted_at, :ended_at, :detail))
+    redirect_to '/calendar'
+  end
+
 
   def deleteConfirm #GOTO
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 #  get 'schedule/:schedule_id' => 'access_schedules#getSchedule'
 
 # 既存スケジュールの変更、calendars#inputScheduleに含められない場合、作成予定
-#  get 'schedule/:schedule_id/edit' => 'access_schedules#updateSchedule'
+  get 'schedule/:schedule_id' => 'calendars#edit'
+  put 'schedule/:schedule_id' => 'calendars#update'
 
 # スケジュールの新規登録
   get 'schedule' => 'calendars#inputSchedule'
