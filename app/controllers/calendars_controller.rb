eaa19b664ebbfba086522a93944edfff5d356b86
kegot <510kei.goto@gmail.com>
2020-04-27 00:23:32 +0900

    modify id using day number

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -31,27 +31,35 @@ class CalendarsController < AccessSchedulesController
   end
 
   def registerSchedule
-    userId = session[:user]["id"]
-    if params[:kbn] == "add"
-      insertSchedule = Hash.new
-      insertSchedule[:user_id] = userId
-      insertSchedule[:date] = params[:date]
-      insertSchedule[:title] = params[:title]
-      insertSchedule[:started_at] = params[:started_at]+":00"
-      insertSchedule[:ended_at] = params[:ended_at]+":00"
-      insertSchedule[:detail] = params[:detail]
-      AccessSchedulesController.new.insertSchedule(insertSchedule)
-    elsif params[:kbn] == "edit"
-      updateSchedule = Hash.new
-      updateSchedule[:user_id] = userId
-      updateSchedule[:id] = params[:schedule_id]
-      updateSchedule[:date] = params[:date]
-      updateSchedule[:title] = params[:title]
-      updateSchedule[:started_at] = params[:started_at][0..4]+":00"
-      updateSchedule[:ended_at] = params[:ended_at][0..4]+":00"
-      updateSchedule[:detail] = params[:detail]
-      AccessSchedulesController.new.updateSchedule(updateSchedule)
-    end
+    insertSchedule = Hash.new
+    insertSchedule[:user_id] = userId
+    insertSchedule[:date] = params[:date]
+    insertSchedule[:title] = params[:title]
+    insertSchedule[:started_at] = params[:started_at]+":00"
+    insertSchedule[:ended_at] = params[:ended_at]+":00"
+    insertSchedule[:detail] = params[:detail]
+    AccessSchedulesController.new.insertSchedule(insertSchedule)
+#    userId = session[:user]["id"]
+#    if params[:kbn] == "add"
+#      insertSchedule = Hash.new
+#      insertSchedule[:user_id] = userId
+#      insertSchedule[:date] = params[:date]
+#      insertSchedule[:title] = params[:title]
+#      insertSchedule[:started_at] = params[:started_at]+":00"
+#      insertSchedule[:ended_at] = params[:ended_at]+":00"
+#      insertSchedule[:detail] = params[:detail]
+#     AccessSchedulesController.new.insertSchedule(insertSchedule)
+#    elsif params[:kbn] == "edit"
+#      updateSchedule = Hash.new
+#      updateSchedule[:user_id] = userId
+#      updateSchedule[:id] = params[:schedule_id]
+#      updateSchedule[:date] = params[:date]
+#      updateSchedule[:title] = params[:title]
+#      updateSchedule[:started_at] = params[:started_at][0..4]+":00"
+#      updateSchedule[:ended_at] = params[:ended_at][0..4]+":00"
+#      updateSchedule[:detail] = params[:detail]
+#      AccessSchedulesController.new.updateSchedule(updateSchedule)
+#    end
     redirect_to '/calendar'
   end
 
