3454d5c3512c47a43827a3e61891466237e435ac
tomo0119shige <tomo0119shige@gmail.com>
2020-05-15 00:39:46 +0900

    minor change -validation message of login failed-

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -16,19 +16,18 @@ class AuthenticationsController < ApplicationController
     @request_auth[:password] = password
 
     unless validation(email, password)
-      flash.now[:alert] = 'Email/Passwordを入力してください。'
       return render :login
     end
 
     user = User.find_by(email: email)
-    puts user
-
     if user
       if user.authenticate(password)
         session[:user] = user
+        # 該当ユーザのfails_countを0に戻す
         redirect_to '/calendar'
       else
         flash.now[:alert] = 'Email または Password が違います。'
+        # 該当ユーザのfails_countをインクリメント
         render :login
       end
     else
@@ -41,10 +40,14 @@ class AuthenticationsController < ApplicationController
     flag = true
     if email == ''
       flag = false
-    end
-    if password == ''
-      # flash.now[:alert] = 'passwordを入力してください'
+      if password == ''
+        flash.now[:alert] = 'Email/Password を入力してください。'
+      else
+        flash.now[:alert] = 'Email を入力してください。'
+      end
+    elsif password == ''
       flag = false
+      flash.now[:alert] = 'Passwordを入力してください。'
     end
     flag
   end
